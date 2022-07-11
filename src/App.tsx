afe59f644ae7eca0c8be327cb10921685500b429
Alin Chiosa <alin.c.wiredelta@gmail.com>
2022-07-11 14:18:30 +0200

    chore: added redirect to admin panel if user is admin and the view is bigger than 728 px

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
 import React, { useEffect, useState } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import { Layout, Input, Typography } from 'antd';
 import Home from './pages/Home';
 import NewOrder from './pages/NewOrder';
 import ConfirmingOrder from './pages/ConfirmingOrder';
 import { Login } from './pages/Login';
 import UserContext, { UserType } from './contexts/UserContext';
+import { AdminPanel } from './pages/AdminPanel';
 
 const { Title } = Typography;
 const { Content } = Layout;
 
 const App: React.FC = () => {
   const [userData, setUserData] = useState<UserType>();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (userData?.role === 'admin' && window.innerWidth >= 728) {
+      navigate('/admin-panel');
+    }
+  }, [userData]);
 
   return (
     <div className='App'>
@@ -32,6 +40,7 @@ const App: React.FC = () => {
               )
             }
           />
+          <Route path='/admin-panel' element={<AdminPanel />} />
           <Route path='/new-order' element={<NewOrder barName='Space Bar' />} />
           <Route path='/confirming-order' element={<ConfirmingOrder />} />
         </Routes>
---
+++ src/pages/AdminPanel.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const AdminPanel: React.FC = ({}) => {
+  return <div></div>;
+};
